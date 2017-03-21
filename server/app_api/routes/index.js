df9b819bda7b822b31851af22544df60361c54fa
Marwen <landoulsi_marwen@hotmail.com>
2017-03-21 14:52:01 +0100

    new version v0.0.7

--- server/app_api/routes/index.js
+++ server/app_api/routes/index.js
@@ -242,6 +242,7 @@ get('/me', ctrlProxy.get);
 
 put('/place/:placeId/folder/:folderId/file/:fileId/version/:version/approve', ctrlProxy.put);
 put('/place/:placeId/folder/:folderId/file/:fileId', ctrlProxy.put);
+put('/place/:placeId/folder/:folderId',ctrlProxy.put);
 put('/user/close-postit', ctrlProxy.put);
 module.exports = router;
 
